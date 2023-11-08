bfd9326cf3abf3435be5ccd8ea57e044cc96537c
RomeoLeyn <romanpituh228@gmail.com>
2023-11-08 10:26:07 +0200

    lab 3 fix

--- lab3/src/main/java/org/example/Main.java
+++ lab3/src/main/java/org/example/Main.java
@@ -3,22 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        String str = "Ввести текст, що складається з декількох слів. В тексті після літери Р (або \n" +
+        String text = "Ввести текст, що складається з декількох слів. В тексті після літери Р (або \n" +
                 "р), якщо вона не остання в слові, може зустрічатися літера А (або а). У такому \n" +
                 "випадку замінити її на літеру О (або о). Вивести початковий та змінений рядок.\n";
 
-        TextChanger textChanger = new TextChanger(str, "якщо", "!!!");
 
-        StringBuilder changedText = textChanger.insertWordAtIndex();
-        System.out.println(changedText);
+        StringBuilder changedText = TextChanger.insertWordAtIndex(text, "що", "!!!!!!!!!!");
+        System.out.println("Текст після редагування:  \n" + changedText);
 
-        boolean check = textChanger.validPassword("PAWSgfgWD-a");
+        String password = "PAWSgfgWD2-a";
+        boolean check = TextChanger.validPassword(password);
+
         if (check) {
-            System.out.println("your password validate");
+            System.out.println("Your password validate \n");
         } else {
             System.out.println("Your password does not have 8 characters, the presence of capital letters and\n" +
                     "lowercase letters, numbers and special characters");
         }
-        System.out.println(check);
     }
 }
--- lab3/src/main/java/org/example/TextChanger.java
+++ lab3/src/main/java/org/example/TextChanger.java
@@ -4,54 +4,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextChanger {
-    private String text;
-    private String findWord;
-    private String insertWord;
-    private int index;
+    public static StringBuilder insertWordAtIndex(String text, String findWord, String insertWord) {
 
-    public TextChanger(String text, String findWord, String insertWord) {
-        this.text = text;
-        this.findWord = findWord;
-        this.insertWord = insertWord;
-    }
+        int index;
 
-    public String getText() {
-        return text;
-    }
+        StringBuilder stringBuilder = new StringBuilder(text);
+        index = text.indexOf(findWord);
 
-    public String getFindWord() {
-        return findWord;
-    }
-
-    public String getInsertWord() {
-        return insertWord;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public void setFindWord(String findWord) {
-        this.findWord = findWord;
-    }
-
-    public void setInsertWord(String insertWord) {
-        this.insertWord = insertWord;
-    }
-
-    public StringBuilder insertWordAtIndex() {
-
-        StringBuilder stringBuilder = new StringBuilder(this.text);
-        this.index = this.text.indexOf(this.findWord);
-
-        if (!(this.index == 0)) {
-            stringBuilder.insert(this.index + this.findWord.length() + 1, this.insertWord);
+        if (!(index == 0)) {
+            stringBuilder.insert(index + findWord.length() + 1, insertWord);
         }
 
         return stringBuilder;
     }
 
-    public boolean validPassword(String password) {
+    public static boolean validPassword(String password) {
         Pattern checkUpperCase = Pattern.compile("[A-Z]+");
         Pattern lowerCasePattern = Pattern.compile("[a-z]+");
         Pattern specialSymbolPattern = Pattern.compile("[\\W]");
